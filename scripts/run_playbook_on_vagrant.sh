34593a4f6c19806e06d4d9fd1a4124e4e325a830
spring-raining <fdonthavei@gmail.com>
2016-09-14 10:18:07 +0900

    Docker

--- scripts/run_playbook_on_vagrant.sh
+++ scripts/run_playbook_on_vagrant.sh
@@ -2,6 +2,7 @@
 
 set -ex
 CURRENT=$(cd $(dirname $0) && pwd)
+cd $CURRENT
 
-ansible-playbook -u vagrant --private-key=~/.vagrant.d/insecure_private_key -i ./hosts ./odango_vagrant.yml -vvv
+ansible-playbook -u vagrant --private-key=~/.vagrant.d/insecure_private_key -i ./../hosts ./../odango_vagrant.yml -vvv -K
 
